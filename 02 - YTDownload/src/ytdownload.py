2e7b0dc082c2b81bb20315aa621b2cc7da0766fb
igormanoels <s.igormanoel@gmail.com>
2024-12-26 22:11:32 -0300

    Iniciando os códigos do projeto.

---
+++ 02 - YTDownload/src/ytdownload.py
@@ -0,0 +1,96 @@
+from tkinter import *
+from tkinter import messagebox
+
+from pytube import YouTube
+#from pytube import *
+
+from os import *
+
+# Antes do programa iniciar ele deve descobrir se o sistema é windows ou linux, pra definir o local padrão
+# Variáveis Globais
+SISTEMA = 'windows'
+LOCAL = None
+FORMATO = 0
+LINK = None
+
+
+# Cores
+preto = "#1E1E1E"
+laranja = "#DF9C57"
+branco = "#F1F1F1"
+vermelho = "#721B1B"
+
+
+# Funções
+def buscarDiretorio():
+    pass
+
+def alteraFormatoMP4():
+    global FORMATO, LOCAL
+    FORMATO = 'MP4'
+    LOCAL = "C://Users//Public//Videos//YT Download//Videos"
+
+def alteraFormatoMP3():
+    global FORMATO, LOCAL
+    FORMATO = 'MP3'
+    LOCAL = "C://Users//Public//Videos//YT Download//Audio"
+
+def iniciarDownload():
+    global LINK
+    LINK = etLink.get()
+
+    if FORMATO == 0:
+        messagebox.showinfo("Atenção", "Antes de inciar o download selecione um formato.")
+        return
+
+    if LINK is None:
+        print(LINK)
+        messagebox.showinfo("Atenção", "Antes de inciar o download informe o link desejado.")
+        return
+    try:
+        link = "https://www.youtube.com/watch?v=MVJMfigEON4"
+        yt = YouTube(link)
+        video = yt.streams.filter(res="720p", file_extension="mp4").first()
+        video.download(output_path="C:/Downloads", filename="meu_video.mp4")
+        print("Download concluído!")
+    except Exception as e:
+        messagebox.showerror("Erro", f"Ocorreu um erro: {e}")
+
+
+
+# Propriedades da Tela
+tela = Tk()
+tela.title('YT Download')
+#tela.iconphoto(False, PhotoImage(file='/docs/figma/design system/icon-janela.png'))
+tela.geometry('720x480')
+tela.resizable(width=False, height=False)
+tela.config(background=branco, border=False)
+
+
+## Componentes da tela
+
+#lbLogo = Label(tela, image='D://GitHub//.Meus-Projetos//02 - YTDownload//docs//figma//design system//Logo final.png').place(x=255, y=44)
+
+etDestino = Entry(tela, width=35, font=('Viga 16'))
+etDestino.insert(0, 'Informe aqui a pasta de destino')
+etDestino.place(x=80, y=183)
+
+btnProcurar = Button(tela, text='Procurar', font='Viga', bg=branco, fg=laranja, width=12, height=2)
+btnProcurar.place(x=520, y=187)
+
+etLink = Entry(tela, width=46, font=('Viga 16'))
+etLink.insert(0, 'Insira seu link aqui!')
+etLink.place(x=80, y=247)
+
+btnFormato4 = Button(tela, command=alteraFormatoMP4, text='MP4', font='Viga', bg=branco, fg=laranja, width=12, height=2)
+btnFormato4.place(x=248, y=311)
+
+btnFormato3 = Button(tela, command=alteraFormatoMP3, text='MP3', font='Viga', bg=branco, fg=laranja, width=12, height=2)
+btnFormato3.place(x=384, y=311)
+
+btnDonwload = Button(tela, command=iniciarDownload, text='Download', font='Viga', bg=branco, fg=laranja, width=12, height=2)
+btnDonwload.place(x=520, y=311)
+
+
+
+tela.mainloop()
